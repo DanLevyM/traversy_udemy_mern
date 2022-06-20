4811c9886844498beb6d65dbc5b7a4bd90f4d08f
DanLevyM <dan.levy.pro@gmail.com>
2022-06-20 23:11:23 +0200

    route

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -1,10 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
+import Card from './components/shared/Card';
 
 import FeedbackData from './data/FeedBackdata';
 
@@ -22,14 +30,39 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header text='Feedback UI' />
       <div className='container'>
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+                <AboutIconLink />
+              </>
+            }
+          ></Route>
+
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+
+        <Card>
+          <NavLink to='' activeClassName='active'>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeClassName='active'>
+            About
+          </NavLink>
+        </Card>
       </div>
-    </>
+    </Router>
   );
 }
 
